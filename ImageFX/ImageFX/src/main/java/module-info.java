118fd8435701c0890a5fd51d6d46ce56abd3f4bb
NoahMcMullan <noahmcmullan20@gmail.com>
2025-02-17 16:03:10 -0600

    Delete ImageFX/ImageFX directory

--- ImageFX/ImageFX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.okcu.imagefx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens edu.okcu.imagefx to javafx.fxml;
-    exports edu.okcu.imagefx;
-}
